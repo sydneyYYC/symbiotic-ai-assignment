b492ff6f7c76f4ae2b76cfcac03dc41626d005cc
Sydney Bruce <sydneybruce20@gmail.com>
2024-01-25 14:38:24 -0700

    added first section with placeholder images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,23 @@
 
 <!-- hero section -->
 <header class="font-inter text-white bg-black h-[30%] flex flex-col items-center justify-center">
-	<div>
-		<h1 class="text-6xl font-bold">Take control of Your Heart. Your health.</h1>
-		<p class="block mt-2">Powered by Symbiotic AI</p>
+	<div class="my-6">
+		<h1 class="text-6xl font-bold">Take control of Your Heart. Your Health.</h1>
+		<p class="block my-2">Powered by Symbiotic AI</p>
 	</div>
 </header>
-<!-- first section  -->
-<section>
-	<section class="w-[50%] h-auto mx-8">
-		<h3>We turn big data into better health</h3>
-		<p>Here at Symbiotic AI we are harnessing the potential of artificial intelligence to empower clinicians and patients making healthcare decisions.</p>
+<main class="mx-3 sm:mx-6 md:mx-[5rem]">
+	<!-- first section  -->
+	<section class="flex space-between flex-wrap mt-4">
+		<section class="w-[50%] h-auto self-start leading-10">
+			<h3 class="text-6xl block my-4 font-semibold">We turn big data into better health.</h3>
+			<p class="text-xl max-w-[50ch] block my-6">Here at Symbiotic AI we are harnessing the potential of artificial intelligence to empower clinicians and patients making healthcare decisions.</p>
+			<a href="/about" class="block max-w-max"><p class="my-6 bg-blue max-w-max px-2 py-[0.1rem] rounded-full text-white font-semibold text-md hover:bg-darkblue">Discover More</p></a>
+		</section>
+		<!-- overlapping images -->
+		<section class="w-[50%] h-auto self-end my-4 grid grid-cols-3 grid-rows-2">
+			<img class="w-[442px] h-[365px] bg-slate-200 z-10 rounded-lg col-span-3 row-start-1 col-start-1" src="" alt="placeholder">
+			<img class="w-[288px] h-[247px] bg-slate-500 rounded-lg col-start-2 col-span-3 row-start-1 row-span-2 self-center" src="" alt="smaller placeholder">
+		</section>
 	</section>
-	<!-- overlapping images -->
-	<section>
-
-	</section>
-</section>
+</main>
