2e79c878e0477aa125ad5bd57a3478e36487244a
Sydney Bruce <sydneybruce20@gmail.com>
2024-01-27 13:39:21 -0700

    added mobile styles, responsive text and alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,42 +6,42 @@
 
 <!-- hero section -->
 <header class="font-inter text-white bg-black h-[30%] flex flex-col items-center justify-center">
-	<div class="my-6 p-4">
-		<h1 class="text-6xl font-bold">Take control of Your Heart. Your Health.</h1>
-		<p class="block my-2">Powered by Symbiotic AI</p>
+	<div class="my-6 p-4 mr-2 md:m-auto">
+		<h1 class="text-4xl md:text-6xl font-bold ">Take control of Your Heart. Your Health.</h1>
+		<p class="md:text-xl block my-2">Powered by Symbiotic AI</p>
 	</div>
 </header>
-<main class="mx-3 sm:mx-6 md:mx-[5rem]">
+<main class="mx-3 sm:mx-6 md:mx-[5rem] mx-6">
 	<!-- first section including text and images-->
-	<section class="flex justify-between flex-wrap mt-4">
-		<section class="w-[50%] h-auto self-start leading-10">
-			<h3 class="text-6xl block my-4 font-semibold">We turn big data into better health.</h3>
+	<section class="flex md:justify-between flex-wrap mt-4 justify-items-stretch">
+		<section class="md:w-[50%] h-auto self-start leading-10">
+			<h3 class=" text-4xl md:text-6xl block my-4 font-semibold">We turn big data into better health.</h3>
 			<p class="text-xl max-w-[50ch] block my-6">Here at Symbiotic AI we are harnessing the potential of artificial intelligence to empower clinicians and patients making healthcare decisions.</p>
 			<a href="/about" class="block max-w-max"><p class="my-6 bg-blue max-w-max px-2 py-[0.1rem] rounded-full text-white font-semibold text-md hover:bg-darkblue">Discover More</p></a>
 		</section>
 		<!-- overlapping images -->
-		<section class="w-[50%] h-auto my-4 grid grid-cols-4 grid-rows-4">
-			<img class="w-[442px] h-[365px] bg-slate-200 z-10 rounded-lg col-span-3 row-start-1 col-start-1 row-span-3" src="" alt="placeholder">
-			<img class="w-[288px] h-[247px] bg-slate-500 rounded-lg col-start-3 col-span-3 row-start-3 row-span-2 self-end" src="" alt="smaller placeholder">
+		<section class=" w-[0] md:w-[50%] h-auto my-4 grid grid-cols-4 grid-rows-4">
+			<img class="md:w-[442px] md:h-[365px] w-[0] h-[0] bg-slate-200 z-10 rounded-lg col-span-3 row-start-1 col-start-1 row-span-3" src="" alt="placeholder">
+			<img class="md:w-[288px] md:h-[247px] bg-slate-500 w-[0] h-[0] rounded-lg col-start-3 col-span-3 row-start-3 row-span-2 self-end" src="" alt="smaller placeholder">
 		</section>
 	</section>
 	<!-- second section of two columns -->
 	<section>
-		<h3 class="text-5xl font-semibold block my-8">The future of health</h3>
-		<div class="flex flex-wrap justify-between items-start my-12">
-			<div class="flex flex-col max-w-[45%] items-start">
+		<h3 class="text-4xl md:text-5xl font-semibold block my-8">The future of health</h3>
+		<div class="flex flex-wrap md:justify-between items-start my-12 ">
+			<div class="flex flex-col md:max-w-[45%] items-start min-w-screen">
 				<img class="h-[80px] w-auto mb-4" src="/assets/ai-brain-logo.svg" alt="brain representing Artificial Intelligence">
-				<p class="text-xl">Pioneering the future of healthcare through advanced data analytics, machine learning, and artificial intelligence. Recognizing the transformative power of artificial intelligence, we are charting the course for a healthcare landscape powered by the limitless potential of artificial intelligence.</p>
+				<p class="text-lg md:text-xl">Pioneering the future of healthcare through advanced data analytics, machine learning, and artificial intelligence. Recognizing the transformative power of artificial intelligence, we are charting the course for a healthcare landscape powered by the limitless potential of artificial intelligence.</p>
 			</div>
-			<div class="flex flex-col max-w-[45%] items-start">
+			<div class="flex flex-col md:max-w-[45%] items-start min-w-screen mt-4 md:mt-0">
 				<img class="h-[90px] w-auto" src="/assets/heart-logo.svg" alt="heart representing heart health">
-				<p class="text-xl">Our collective vision is to unlock the true potential hidden within digital health data, propelling it from just data to solutions that actually improve healthcare practices. Fuelling this vision is a diverse and talented team dedicated to pushing the boundaries of digital health.</p>
+				<p class="text-lg md:text-xl">Our collective vision is to unlock the true potential hidden within digital health data, propelling it from just data to solutions that actually improve healthcare practices. Fuelling this vision is a diverse and talented team dedicated to pushing the boundaries of digital health.</p>
 			</div>
 		</div>
 	</section>
 	<!-- values section -->
 	<section class="my-8">
-		<h4 class="text-4xl font-semibold">Serving our community</h4>
+		<h4 class="text-3xl md:text-4xl font-semibold">Serving our community</h4>
 		<p>Here at Symbiotic AI we promise to serve with:</p>
 		<!-- values boxes flex container -->
 		<div class="flex justify-between mt-8 flex-wrap">
@@ -71,28 +71,28 @@
 		</div>
 	</section>
 	<!-- our team section -->
-	<section class="my-8 mt-[6rem]">
-		<h3 class="font-semibold text-4xl my-8 mb-[4rem]">Our Team of Experts</h3>
+	<section class=" my-8 mt-[6rem]">
+		<h3 class="font-semibold text-3xl md:text-4xl my-8 mb-[4rem]">Our Team of Experts</h3>
 		<div class="flex justify-between flex-wrap">
-			<div class="flex flex-col items-center">
+			<div class="flex flex-col items-center mb-8 md:mb-0 ml-12 md:ml-0">
 				<img class="h-[250px] w-[250px] rounded-full bg-slate-200 mb-4" src="/" alt="Mr. Arjun Puri">
 				<h5 class="font-semibold text-xl">Chief Executive Officer</h5>
 				<p >Mr. Arjun Puri</p>
 			</div>
-			<div class="flex flex-col items-center">
+			<div class="flex flex-col items-center mb-8 md:mb-0 ml-12 md:ml-0">
 				<img class="h-[250px] w-[250px] rounded-full bg-slate-200 mb-4" src="/" alt="Dr. Joon Lee">
 				<h5 class="font-semibold text-xl">Chief Technology Officer</h5>
 				<p>Dr. Joon Lee</p>
 			</div>
-			<div class="flex flex-col items-center">
+			<div class="flex flex-col items-center mb-8 md:mb-0 ml-12 md:ml-0">
 				<img class="h-[250px] w-[250px] rounded-full bg-slate-200 mb-4" src="/" alt="Ms. Emma Bogner">
 				<h5 class="font-semibold text-xl" >Product Manager</h5>
 				<p>Ms. Emma Bogner</p>
 			</div>
 		</div>
 	</section>
-	<section class="flex flex-col items-center my-[8rem]">
-		<h3 class="font-bold text-5xl mb-4">Join us on the journey.</h3>
+	<section class="flex flex-col items-center my-[6rem] md:my-[8rem] min-w-screen">
+		<h3 class=" text-3xl font-bold md:text-5xl mb-4">Join us on the journey.</h3>
 		<ContactButton />
 	</section>
 </main>
